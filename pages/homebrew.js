0178ba8f40dd2873ca8cc6a8ae372fd54dddf882
Minsi Yang <yangminsi@gmail.com>
2020-10-22 13:08:07 +0100

    Map out the basic HTML for homebrew starting page

--- pages/homebrew.js
+++ pages/homebrew.js
@@ -1,9 +1,29 @@
-
+import Head from 'next/head'
 
 export default function Homebrew() {
   return (
     <div>
+      <Head>
+        <title>Homebrew</title>
+      </Head>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>OS X</li>
+          <li>Essential</li>
+          <li>Homebrew</li>
+          <li>2 - Press Enter</li>
+        </ul>
+      </div>
       <h1>Homebrew</h1>
+      <hr />
+      <h2>2 - Press Enter</h2>
+      <h3>The instruction:</h3>
+      <p>"Press RETURN to continue or any other key to abort"</p>
+      <h3>Asks you to press:</h3>
+      <p>Enter</p>
+      <h3>Next step >></h3>
+      <hr />
     </div>
   )
 }
